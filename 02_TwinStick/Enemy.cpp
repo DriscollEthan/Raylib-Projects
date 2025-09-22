0651176f8ae3bec34d24ef6f676f326f04117631
DriscollEthan <158096741+DriscollEthan@users.noreply.github.com>
2025-09-22 13:01:06 -0700

    Setup for Enemy

---
+++ 02_TwinStick/Enemy.cpp
@@ -0,0 +1,26 @@
+#include "Enemy.h"
+
+Enemy::Enemy(Driscoll::Vector2D _position, raylib::Image _texture, Driscoll::Vector2D _origin, Driscoll::Vector2D _scale, float _radius, float _rotation, float _speed) : Entity(_position, _texture, _origin, _scale, _radius, _rotation, _speed)
+{
+
+}
+
+Enemy::~Enemy()
+{
+
+}
+
+void Enemy::BeginPlay()
+{
+
+}
+
+void Enemy::Update()
+{
+
+}
+
+void Enemy::Draw()
+{
+
+}
---
+++ 02_TwinStick/Enemy.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Entity.h"
+
+class Enemy : public Entity
+{
+	/* CONSTRUCTORS & DESTRUCTORS */
+		/**
+			* DEFAULT CONSTRUCTOR:
+			* Parameters:
+			* Position as a Driscoll::Vector2D	| Default: 0,0
+			* Texture as an Image								| Default: Default Constructor
+			* Radius as a Float									| Default: 0.0
+			* Rotation as a Float								| Default: 0.0
+			* Speed as a flaot									| Default: 1.0
+			*/
+	Enemy(Driscoll::Vector2D _position = { 0,0 }, raylib::Image _texture = {}, Driscoll::Vector2D _origin = { 0,0 }, Driscoll::Vector2D _scale = { 1,1 }, float _radius = 0.0f, float _rotation = 0.0f, float _speed = 1.0f);
+
+  ~Enemy();
+
+  virtual void BeginPlay() override;
+
+  virtual void Update() override;
+
+  virtual void Draw() override;
+
+
+};
+
