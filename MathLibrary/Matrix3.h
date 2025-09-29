3a1428dc2924fed2976d50651c793a362ebe10be
DriscollEthan <158096741+DriscollEthan@users.noreply.github.com>
2025-09-29 15:18:55 -0700

    Removed useless comments

--- MathLibrary/Matrix3.h
+++ MathLibrary/Matrix3.h
@@ -18,18 +18,6 @@ namespace Driscoll
 		 * m2 m5 m8
 		 * m3 m6 m9
 		 */
-		/*	m1 m5 m9  m13
-			* m2 m6 m10 m14
-			* m3 m7 m11 m15
-			* m4	m8 m12 m16
-			* 
-			* m1  m2  m3  m4
-			* m5  m6  m7  m8
-			* m9  m10 m11 m12
-			* m13 m14 m15 m16
-			* 
-			*/
-
 		union
 		{
 			struct
